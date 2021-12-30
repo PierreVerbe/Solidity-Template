d9bb16f70c1d3ea66db61658aa136153c46db368
PierreVerbe <pierre.verbe@free.fr>
2021-12-30 15:47:40 +0100

    Added Greeter

---
+++ greeter/contracts/Greeter.sol
@@ -0,0 +1,16 @@
+pragma solidity >=0.4.22 <0.9.0;
+
+contract Greeter {
+    string private _greeting = "Hello, World!";
+    address private _owner;
+
+    function greet() external view returns(string memory) { 
+        return _greeting;
+    }
+    function setGreeting(string calldata greeting) external { 
+        _greeting = greeting;
+    }
+    function owner() public view returns(address) { 
+        return _owner;
+    }
+}
--- greeter/test/greeter_test.js
+++ greeter/test/greeter_test.js
@@ -3,6 +3,39 @@ const GreeterContract = artifacts.require("Greeter");
 contract("Greeter", () => {
     it("has been deployed successfully", async () => {
         const greeter = await GreeterContract.deployed();
-        assert(greeter, "contract was not deployed");
+        assert(greeter, "contract failed to deploy");
     });
+
+    describe("greet()", () => {
+        it("returns 'Hello, World!'", async () => {
+            const greeter = await GreeterContract.deployed();
+            const expected = "Hello, World!";
+            const actual = await greeter.greet();
+
+            assert.equal(actual, expected, "greeted with 'Hello, World!'");
+        })
+    });
+
+  describe("owner()", () => {
+        it("returns the address of the owner", async () => {
+            const greeter = await GreeterContract.deployed();
+            const owner = await greeter.owner();
+
+            assert(owner, "the current owner");
+        });
+    }); 
+})
+
+contract("Greeter: update greeting", () => {
+    describe("setGreeting(string)", () => {
+        it("sets greeting to passed in string", async () => { 
+            const greeter = await GreeterContract.deployed();
+            const expected = "Hi there!";
+
+            await greeter.setGreeting(expected);
+            const actual = await greeter.greet();
+
+            assert.equal(actual, expected, "greeting was not updated");
+        });
+    }); 
 });
